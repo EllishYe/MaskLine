4dff24adbf3fea3eb161fdc7a977b2d3a0767eca
Ellish <ellishyeh@126.com>
2026-01-31 12:30:12 +0000

    Drag system | V1.1 drag single and drag all complete

---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Drag: MonoBehaviour
+{
+    private bool isDaragging = false;
+    private Vector3 offset;
+
+    
+
+    void Update()
+    {
+        if (isDaragging)
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+    }
+
+    private void OnMouseDown()
+    {
+        isDaragging = true;
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+    private void OnMouseUp()
+    {
+        isDaragging = false;
+        
+    }
+}
